a9db8235486d9ac8a299428734249c717dda735b
UebuNogamy <georgiewich.sergey@gmail.com>
2024-01-14 02:46:26 +0500

    Удалена функциональность погоды.

--- src/commands/FunctionCalls.java
+++ src/commands/FunctionCalls.java
@@ -1,6 +1,5 @@
 package commands;
 
-import java.util.Arrays;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -12,25 +11,7 @@ import org.eclipse.e4.core.di.annotations.Creatable;
 public class FunctionCalls
 {
     @Inject
-    private OpenWeatherCommand openWeatherCommand;
-    @Inject
     private ReadJavaDocCommand readJavaDocCommand;
-    
-    @Function(name="getCurrentWeather", description="Get the current weather in a given location", type="object")
-    public String getCurrentWeather( 
-            @FunctionParam(name="location", description="The city and state or country, e.g. San Francisco, CA", required=true) String location, 
-            @FunctionParam(name="unit", description="The temperature unit, e.g. metric or imperial. Default value: metric" ) String unit )
-    {
-        var userUnit = unit;
-        unit = Arrays.stream( new String[]{"imperial", "metric", "standard"} )
-                     .filter( a -> a.equals( userUnit ) )
-                     .findAny()
-                     .orElse( "metric" );
-            
-        return openWeatherCommand.getCurrentWeather( location, unit );
-    }
-    
-    public record WeatherReport( String location, int degrees, String unit, String[] forecast ) {};
     
     
     @Function(name="getJavaDoc", description="Get the JavaDoc for the given compilation unit.  For example,a class B defined as a member type of a class A in package x.y should have athe fully qualified name \"x.y.A.B\".Note that in order to be found, a type name (or its top level enclosingtype name) must match its corresponding compilation unit name.", type="object")
